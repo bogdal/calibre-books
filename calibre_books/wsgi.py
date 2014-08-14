cc9cd4861291f26a525640c0cd73c2ad0ce8647e
Adam Bogdal <adam@bogdal.pl>
2014-08-14 18:01:10 +0200

    Install dj-static package

--- calibre_books/wsgi.py
+++ calibre_books/wsgi.py
@@ -1,14 +1,7 @@
-"""
-WSGI config for calibre_books project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
 import os
+from dj_static import Cling, MediaCling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calibre_books.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'Pillow==2.5.2',
         'gunicorn==19.1.0',
         'psycopg2==2.5.3',
-
+        'dj-static==0.0.6',
     ],
     entry_points={
         'console_scripts': ['manage.py = calibre_books:manage']},
