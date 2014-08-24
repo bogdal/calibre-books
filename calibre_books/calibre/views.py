ca446e7e9b746e20e14e3290a1a283bea98f948c
Adam Bogdal <adam@bogdal.pl>
2014-08-24 03:13:37 +0200

    Add ability to filter by authors and tags Fix #7

--- calibre_books/calibre/views.py
+++ calibre_books/calibre/views.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.shortcuts import get_object_or_404
+from django.utils.http import urlencode
 from django.views.generic import ListView, RedirectView
 
 from .models import Book, Data
@@ -12,6 +13,21 @@ class BookListView(ListView):
     model = Book
     template_name = 'calibre/list.html'
     paginate_by = 12
+
+    def get_queryset(self):
+        qs = super(BookListView, self).get_queryset()
+        qs_kwargs = {}
+        if 'author' in self.request.GET:
+            qs_kwargs['authors__name__in'] = self.request.GET.getlist('author')
+        if 'tag' in self.request.GET:
+            qs_kwargs['tags__name__in'] = self.request.GET.getlist('tag')
+        return qs.filter(**qs_kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(BookListView, self).get_context_data(*args, **kwargs)
+        context['filters'] = urlencode({'author': self.request.GET.getlist('author'),
+                                        'tag': self.request.GET.getlist('tag')}, doseq=1)
+        return context
 
 
 class DownloadView(RedirectView):
