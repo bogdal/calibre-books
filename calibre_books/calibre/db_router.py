4bae6bae7351a0afd93af89bca49c1748cc7ad37
Adam Bogdał <adam@bogdal.pl>
2015-10-07 22:00:57 +0200

    Don't create calibre tables in main db

--- calibre_books/calibre/db_router.py
+++ calibre_books/calibre/db_router.py
@@ -2,7 +2,6 @@
 class DbRouter(object):
 
     def db_for_read(self, model, **hints):
-
         if model._meta.app_label == 'calibre':
             return 'calibre'
         return 'default'
@@ -11,3 +10,16 @@ class DbRouter(object):
         if model._meta.app_label == 'calibre':
             return None
         return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == 'calibre' or \
+           obj2._meta.app_label == 'calibre':
+           return True
+        return None
+
+    def allow_migrate(self, db, app_label, model=None, **hints):
+        if db == 'calibre':
+            return model._meta.app_label == 'calibre'
+        elif model and model._meta.app_label == 'calibre':
+            return False
+        return None
